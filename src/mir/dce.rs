d52fd090a39253726d27ca22547e22a023973efc
Johan-Mi <johanmi@protonmail.com>
2025-01-06 19:28:40 +0100

    mir: Remove unused return values

--- src/mir/dce.rs
+++ src/mir/dce.rs
@@ -1,7 +1,12 @@
-use super::{Op, ParameterId, Program, Value};
+use super::{Op, ParameterId, Program, ReturnId, Value};
 use slotmap::SecondaryMap;
 
 pub fn perform(program: &mut Program) {
+    parameters(program);
+    returns(program);
+}
+
+fn parameters(program: &mut Program) {
     let used_parameters: SecondaryMap<ParameterId, ()> = program
         .ops
         .values_mut()
@@ -24,6 +29,27 @@ pub fn perform(program: &mut Program) {
     for op in program.ops.values_mut() {
         if let Op::Call { arguments, .. } = op {
             arguments.retain(|it, _| used_parameters.contains_key(it));
+        }
+    }
+}
+
+fn returns(program: &mut Program) {
+    let used_returns: SecondaryMap<ReturnId, ()> = program
+        .ops
+        .values_mut()
+        .flat_map(Op::args_mut)
+        .filter_map(|&mut it| {
+            if let Value::Returned { id, .. } = it {
+                Some((id, ()))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    for op in program.ops.values_mut() {
+        if let Op::Return(values) = op {
+            values.retain(|it, _| used_returns.contains_key(it));
         }
     }
 }
