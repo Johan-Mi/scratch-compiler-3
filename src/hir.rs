0c91b0202b56d12963a4b8c06bbc62302e6ec5bb
Johan-Mi <johanmi@protonmail.com>
2025-07-22 16:14:31 +0200

    Revert "hir: Add sprites"
    Sprites don't need to be part of the HIR but can instead be passed
    directly to codegen.
    
    This reverts commit a8d850999cc615092e403cff10dfa27a7ce7296c.

--- src/hir.rs
+++ src/hir.rs
@@ -2,16 +2,10 @@ mod lowering;
 
 pub use lowering::lower;
 
-use crate::ast;
-
-#[derive(Default)]
-pub struct Program {
-    sprites: Vec<ast::Sprite>,
-}
+pub struct Program;
 
 impl Program {
     pub fn merge(&mut self, other: Self) {
-        let Self { sprites } = other;
-        self.sprites.extend(sprites);
+        todo!()
     }
 }
