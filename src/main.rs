45a149f4bfbc134a702feb3fb213251893df623c
Johan-Mi <johanmi@protonmail.com>
2025-08-02 16:31:00 +0200

    Move lint configuration to `Cargo.toml`

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-#![forbid(unsafe_code)]
-#![deny(
-    clippy::allow_attributes,
-    clippy::allow_attributes_without_reason,
-    clippy::let_underscore_untyped,
-    unused_results
-)]
-#![warn(clippy::nursery, clippy::pedantic)]
-
 mod ast;
 mod codegen;
 #[cfg(debug_assertions)]
