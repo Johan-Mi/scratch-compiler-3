620eba4563a5bbbb75adddfcde8577a3893434bd
Johan-Mi <johanmi@protonmail.com>
2026-03-12 14:20:21 +0100

    mir: Create `lowering::Context`
    Passing more parameters by hand seems annoying.

--- src/mir/lowering.rs
+++ src/mir/lowering.rs
@@ -3,7 +3,7 @@ use crate::{mir, parser::K};
 use map::{Id, Map};
 
 pub fn lower(documents: &[cst::Tree<K>]) -> mir::Program {
-    let mut program = mir::Program {
+    let program = mir::Program {
         parameters: Map::default(),
         basic_blocks: Map::default(),
         ops: Map::default(),
@@ -11,6 +11,8 @@ pub fn lower(documents: &[cst::Tree<K>]) -> mir::Program {
         lists: Map::default(),
         returns: Map::default(),
     };
+
+    let mut context = Context { program };
 
     for function_body in documents
         .iter()
@@ -23,73 +25,73 @@ pub fn lower(documents: &[cst::Tree<K>]) -> mir::Program {
         )
         .filter_map(ast::Function::body)
     {
-        lower_block(function_body, &mut program);
+        lower_block(function_body, &mut context);
     }
 
-    program
+    context.program
 }
 
-fn lower_block(block: ast::Block, program: &mut mir::Program) -> Id<mir::BasicBlock> {
-    let basic_block = program.basic_blocks.insert(mir::BasicBlock(Vec::new()));
+struct Context {
+    program: mir::Program,
+}
+
+fn lower_block(block: ast::Block, c: &mut Context) -> Id<mir::BasicBlock> {
+    let basic_block = c.program.basic_blocks.insert(mir::BasicBlock(Vec::new()));
     for statement in block.statements() {
-        lower_statement(statement, basic_block, program);
+        lower_statement(statement, basic_block, c);
     }
     basic_block
 }
 
-fn lower_statement(
-    statement: ast::Statement,
-    basic_block: Id<mir::BasicBlock>,
-    program: &mut mir::Program,
-) {
+fn lower_statement(statement: ast::Statement, basic_block: Id<mir::BasicBlock>, c: &mut Context) {
     match statement {
         ast::Statement::Let(_) => todo!(),
         ast::Statement::If(it) => {
-            let condition = one(lower_expression(it.condition().unwrap(), program));
-            let then = lower_block(it.then().unwrap(), program);
+            let condition = one(lower_expression(it.condition().unwrap(), c));
+            let then = lower_block(it.then().unwrap(), c);
             let r#else = if let Some(else_clause) = it.else_clause() {
                 if let Some(else_if) = else_clause.if_() {
-                    let else_block = program.basic_blocks.insert(mir::BasicBlock(Vec::new()));
-                    lower_statement(ast::Statement::If(else_if), else_block, program);
+                    let else_block = c.program.basic_blocks.insert(mir::BasicBlock(Vec::new()));
+                    lower_statement(ast::Statement::If(else_if), else_block, c);
                     else_block
                 } else {
-                    lower_block(else_clause.block().unwrap(), program)
+                    lower_block(else_clause.block().unwrap(), c)
                 }
             } else {
-                program.basic_blocks.insert(mir::BasicBlock(Vec::new()))
+                c.program.basic_blocks.insert(mir::BasicBlock(Vec::new()))
             };
-            let op = program.ops.insert(mir::Op::If {
+            let op = c.program.ops.insert(mir::Op::If {
                 condition,
                 then,
                 r#else,
             });
-            program.basic_blocks[basic_block].0.push(op);
+            c.program.basic_blocks[basic_block].0.push(op);
         }
         ast::Statement::Repeat(it) => {
-            let times = one(lower_expression(it.times().unwrap(), program));
-            let body = lower_block(it.body().unwrap(), program);
-            let op = program.ops.insert(mir::Op::Repeat { times, body });
-            program.basic_blocks[basic_block].0.push(op);
+            let times = one(lower_expression(it.times().unwrap(), c));
+            let body = lower_block(it.body().unwrap(), c);
+            let op = c.program.ops.insert(mir::Op::Repeat { times, body });
+            c.program.basic_blocks[basic_block].0.push(op);
         }
         ast::Statement::Forever(it) => {
-            let body = lower_block(it.body().unwrap(), program);
-            let op = program.ops.insert(mir::Op::Forever(body));
-            program.basic_blocks[basic_block].0.push(op);
+            let body = lower_block(it.body().unwrap(), c);
+            let op = c.program.ops.insert(mir::Op::Forever(body));
+            c.program.basic_blocks[basic_block].0.push(op);
         }
         ast::Statement::While(_) => todo!(),
         ast::Statement::Until(_) => todo!(),
         ast::Statement::For(_) => todo!(),
         ast::Statement::Return(_) => todo!(),
-        ast::Statement::Expression(it) => assert!(lower_expression(it, program).is_empty()),
+        ast::Statement::Expression(it) => assert!(lower_expression(it, c).is_empty()),
     }
 }
 
-fn lower_expression(expression: ast::Expression, program: &mut mir::Program) -> Vec<mir::Value> {
+fn lower_expression(expression: ast::Expression, c: &mut Context) -> Vec<mir::Value> {
     match expression {
-        ast::Expression::Parenthesized(it) => lower_expression(it.inner().unwrap(), program),
+        ast::Expression::Parenthesized(it) => lower_expression(it.inner().unwrap(), c),
         ast::Expression::Variable(_) => todo!(),
-        ast::Expression::FunctionCall(_) => lower_call(program),
-        ast::Expression::BinaryOperation(_) => lower_call(program),
+        ast::Expression::FunctionCall(_) => lower_call(c),
+        ast::Expression::BinaryOperation(_) => lower_call(c),
         ast::Expression::NamedArgument(_) => todo!(),
         ast::Expression::DecimalNumber(_) => todo!(),
         ast::Expression::BinaryNumber(_) => todo!(),
@@ -100,13 +102,13 @@ fn lower_expression(expression: ast::Expression, program: &mut mir::Program) -> 
         ast::Expression::KwTrue(_) => [mir::Value::Bool(true)].into(),
         ast::Expression::Lvalue(_) => todo!(),
         ast::Expression::ListLiteral(_) => todo!(),
-        ast::Expression::TypeAscription(it) => lower_expression(it.inner().unwrap(), program),
-        ast::Expression::MethodCall(_) => lower_call(program),
+        ast::Expression::TypeAscription(it) => lower_expression(it.inner().unwrap(), c),
+        ast::Expression::MethodCall(_) => lower_call(c),
         ast::Expression::FieldAccess(_) => todo!(),
     }
 }
 
-fn lower_call(program: &mut mir::Program) -> Vec<mir::Value> {
+fn lower_call(c: &mut Context) -> Vec<mir::Value> {
     todo!()
 }
 
